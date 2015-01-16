374a8e8eefe247f8090eda3514cfdefc0fc54b2b
Pauli Niva <Pauli@Vulcan-3.local>
2015-01-16 18:18:36 +0200

    tehtävä 11

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -5,7 +5,7 @@ class BreweriesController < ApplicationController
   # GET /breweries.json
   def index
     @breweries = Brewery.all
-    render :index    # renderöi hakemistosta view/breweries olevan näkymätemplaten index.html.erb
+    render :panimot
   end
 
   # GET /breweries/1
