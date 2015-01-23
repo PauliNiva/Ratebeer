f98cdead1e9239d04ecada5cd26f255131511596
Pauli Niva <Pauli@Vulcan-3.local>
2015-01-23 15:38:38 +0200

    tehtävä 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,11 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    sum = self.ratings.sum :score
+    ratings_count = self.ratings.count
+    average = sum / ratings_count
+    return "#{average}"
+  end
 end
