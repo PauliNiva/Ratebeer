e7308cfa3232599e1f83a1ebfe3de5e0ae556df8
Pauli Niva <Pauli@Vulcan-3.local>
2015-01-25 13:38:56 +0200

    exercise 14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,9 @@
 class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
+
+  def average_rating
+    sum = ratings.map(&:score).inject(:+)
+    (sum / ratings.count.to_f).round(2)
+  end
 end
