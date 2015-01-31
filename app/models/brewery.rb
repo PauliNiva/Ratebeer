e8226812297ab327033f32f60e45816f39f8ebf8
Pauli Niva <Pauli@Vulcan-3.local>
2015-01-31 16:39:48 +0200

    exercise 6

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -5,7 +5,14 @@ class Brewery < ActiveRecord::Base
 
   validates :year, numericality: {
                      only_integer: true,
-                     greater_than_or_equal_to: 1042,
-                     less_than_or_equal_to: 2015
+                     greater_than_or_equal_to: 1042
                  }
+
+  validate :year_not_in_future
+
+  def year_not_in_future
+    if year.present? && year > Time.now.year
+      errors.add(:year, "year cannot be in the future")
+    end
+  end
 end
