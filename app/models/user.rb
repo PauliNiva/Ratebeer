5693c8e730870485d7a20f4bdc8df0eb966e4b62
Pauli Niva <Pauli@Vulcan-3.local>
2015-01-31 13:38:04 +0200

    added ratings to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  has_many :ratings   # user has many ratings
+
+class Rating < ActiveRecord::Base
+  belongs_to :beer
+  belongs_to :user   # rating belongs also to user
+
+  def to_s
+    "#{beer.name} #{score}"
+  end
 end
