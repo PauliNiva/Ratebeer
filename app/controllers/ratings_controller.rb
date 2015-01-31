9e123dc77abf1c715af466fc6da6d5bea31d332b
Pauli Niva <Pauli@Vulcan-3.local>
2015-01-31 14:23:32 +0200

    exercise 3

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -10,17 +10,14 @@ class RatingsController < ApplicationController
   end
 
   def create
-    Rating.create params.require(:rating).permit(:score, :beer_id)
-
-    # save created rating to session
-    session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
-
-    redirect_to ratings_path
+    rating = Rating.create params.require(:rating).permit(:score, :beer_id)
+    current_user.ratings << rating
+    redirect_to current_user
   end
 
   def destroy
     rating = Rating.find(params[:id])
     rating.delete
-    redirect_to ratings_path
+    redirect_to :back
   end
 end
