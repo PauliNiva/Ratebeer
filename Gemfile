0faea21433ff76cc13dbf908c9ca6767aaee831f
Pauli Niva <Pauli@Vulcan-3.local>
2015-02-01 11:12:39 +0200

    exercise 10

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ group :production do
   gem 'rails_12factor'
 end
 
+
 ruby '2.2.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
@@ -31,6 +32,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+
+gem 'bcrypt', '~> 3.1.7'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ActiveRecord::Base
   has_many :beers, through: :ratings
   has_many :beer_clubs, through: :memberships
   has_many :memberships
+
+  has_secure_password
+
+  validates :password, length: {
+                         minimum: 4
+                     }
+  validates_format_of :password, with: /(?=.*[A-ZÅÄÖ])/, message: "Password must contain at least one capital letter"
+  validates_format_of :password, with: /(?=.*[\d])/, message: "Password must contain at least one numerical digit"
 end
