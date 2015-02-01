d23bf4f7408f34284cebba5fd20ae746853eddbb
Pauli Niva <Pauli@Vulcan-3.local>
2015-02-01 13:46:41 +0200

    exercise 13

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   include RatingAverage
   validates :username, uniqueness: true,
                       length: { in: 3..15 }
-  has_many :ratings   # user has many ratings
+  has_many :ratings, dependent: :destroy   # user has many ratings
   has_many :beers, through: :ratings
   has_many :beer_clubs, through: :memberships
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
 
   has_secure_password
 
