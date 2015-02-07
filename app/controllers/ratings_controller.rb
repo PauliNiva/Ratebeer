693f0a9404cb6819d261040ad4d25571a6ba182a
Pauli Niva <Pauli@Vulcan.local>
2015-02-07 20:45:38 +0200

    week 4 exercise 14

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -14,18 +14,19 @@ class RatingsController < ApplicationController
 
     if current_user.nil?
       redirect_to signin_path, notice:'you should be signed in'
-    elsif @rating.save
+    else if @rating.save
       current_user.ratings << @rating
       redirect_to user_path current_user
     else
       @beers = Beer.all
       render :new
     end
-  end
+    end
 
   def destroy
     rating = Rating.find params[:id]
     rating.delete if current_user == rating.user
     redirect_to :back
+    end
   end
 end
