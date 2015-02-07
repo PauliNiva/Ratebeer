c43731e283b1e6997c81de75c3ebd85140f26a33
Pauli Niva <Pauli@Vulcan.local>
2015-02-07 13:32:34 +0200

    week 4 exercise 3

--- Gemfile
+++ Gemfile
@@ -5,8 +5,6 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.0'
 group :development, :test do
   gem 'sqlite3'
-  gem 'better_errors'
-  gem 'rspec-rails', '~> 3.0'
 end
 
 group :production do
@@ -54,5 +52,11 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'better_errors'
+  gem 'rspec-rails', '~> 3.0'
+end
+
+group :test do
+  gem 'factory_girl_rails'
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,13 @@ class User < ActiveRecord::Base
                      }
   validates_format_of :password, with: /(?=.*[A-ZÅÄÖ])/, message: "Password must contain at least one capital letter"
   validates_format_of :password, with: /(?=.*[\d])/, message: "Password must contain at least one numerical digit"
+
+  def favorite_beer
+    return nil if ratings.empty?
+    ratings.order(score: :desc).limit(1).first.beer
+  end
+  def favorite_style
+    return nil if ratings.empty?
+    beers.group(:style).average(:score).max_by { |name, score| score }.first
+  end
 end
