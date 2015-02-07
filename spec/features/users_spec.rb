8d0224c332ce0de365bc30a3199efde899da5268
Pauli Niva <Pauli@Vulcan.local>
2015-02-07 16:29:21 +0200

    week 4 exercise 7

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -19,16 +19,30 @@ describe "User" do
       expect(current_path).to eq(signin_path)
       expect(page).to have_content 'Username and/or password mismatch'
     end
+
+    it "when signed up with good credentials, is added to the system" do
+      visit signup_path
+      fill_in('user_username', with:'Brian')
+      fill_in('user_password', with:'Secret55')
+      fill_in('user_password_confirmation', with:'Secret55')
+
+      expect{ click_button('Create User') }.to change{User.count}.by(1)
+    end
   end
 
-  it "when signed up with good credentials, is added to the system" do
-    visit signup_path
-    fill_in('user_username', with:'Brian')
-    fill_in('user_password', with:'Secret55')
-    fill_in('user_password_confirmation', with:'Secret55')
+describe "who has made ratings" do
+  it "has his ratings on the user page" do
+    user = User.first
+    brewery = FactoryGirl.create :brewery, name:"BrewDog"
+    beer1 = FactoryGirl.create :beer, name:"Nanny State", brewery:brewery
+    beer2 = FactoryGirl.create :beer, name:"Punk IPA", brewery:brewery
+    FactoryGirl.create :rating, beer:beer1, user:user
+    FactoryGirl.create :rating, beer:beer2, user:user
+    visit user_path(user)
 
-    expect{
-      click_button('Create User')
-    }.to change{User.count}.by(1)
+    expect(page).to have_content('Has made 2 ratings')
+    expect(page).to have_content('Nanny State')
+    expect(page).to have_content('Punk IPA')
+    end
   end
 end
