946df955a79332b2222f799d5e117aebd8b34ec9
Pauli Niva <Pauli@Vulcan.local>
2015-02-07 11:38:50 +0200

    week 4 exercise 1

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -11,14 +11,12 @@ class RatingsController < ApplicationController
 
   def create
     @rating = Rating.new params.require(:rating).permit(:score, :beer_id)
-    if current_user
-      if @rating.save
-        current_user.ratings << @rating
-        redirect_to user_path current_user
-      else
-        @beers = Beer.all
-        render :new
-      end
+
+    if current_user.nil?
+      redirect_to signin_path, notice:'you should be signed in'
+    elsif @rating.save
+      current_user.ratings << @rating
+      redirect_to user_path current_user
     else
       @beers = Beer.all
       render :new
