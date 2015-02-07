4b4552cee09fdb48a4b63da689b7d3d38545c482
Pauli Niva <Pauli@Vulcan.local>
2015-02-07 21:01:25 +0200

    week 4 exercise 15

--- spec/features/beer_spec.rb
+++ spec/features/beer_spec.rb
@@ -3,19 +3,26 @@ require 'rails_helper'
 describe "Beer" do
   let!(:brewery) { FactoryGirl.create :brewery, name:"Koff" }
 
+  before :each do
+    FactoryGirl.create :user
+  end
+
   it "can be created with a valid name" do
+    sign_in(username:"Pekka", password:"Foobar1")
     visit new_beer_path
-    fill_in('beer[name]', with: 'olut')
-    expect{
-      click_button('Create Beer')
-    }.to change { Beer.count }.from(0).to(1)
+    save_and_open_page
+    fill_in('beer[name]', with: 'bisse')
+
+    expect{ click_button "Create Beer" }.to change { Beer.count }.from(0).to(1)
     expect(page).to have_content('Beer was successfully created')
   end
 
   it "can not be created with an invalid name" do
+    sign_in(username:"Pekka", password:"Foobar1")
     visit new_beer_path
     fill_in('beer[name]', with: '')
-    click_button('Create Beer')
+    click_button "Create Beer"
+
     expect(page).to have_content("Name can't be blank")
     expect(Beer.count).to eq(0)
   end
