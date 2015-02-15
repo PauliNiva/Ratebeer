434a635032f1102230af7f1bfc25a6132404949c
Pauli Niva <Pauli@Vulcan.local>
2015-02-15 11:32:11 +0200

    week 5 exercise 4

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,6 +1,13 @@
 class BeermappingApi
 
   def self.places_in(city)
+    city = city.downcase
+    Rails.cache.fetch(city, expires_in: 1.week, race_condition_ttl: 1.minute) { fetch_places_in(city) }
+  end
+
+  private
+
+  def self.fetch_places_in(city)
     url = 'http://stark-oasis-9187.herokuapp.com/api/'
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
     places = response.parsed_response["bmp_locations"]["location"]
@@ -9,10 +16,6 @@ class BeermappingApi
     places.inject([]) do | set, place |
       set << Place.new(place)
     end
-  end
-
-  def self.key
-    "nope"
   end
 
 end
